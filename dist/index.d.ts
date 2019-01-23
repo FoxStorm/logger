268a61227f8afb17592c36f79142fe36d8817369
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-01-23 10:42:19 +0000

    Regenerate dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { Logger } from './src/Logger';
 export { PrintLogger } from './src/PrintLogger';
 export { Logging } from './src/Logging';
+export { LogLevel } from './src/LogLevel';
--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,6 @@ var Logger_1 = require("./src/Logger");
 exports.Logger = Logger_1.Logger;
 var PrintLogger_1 = require("./src/PrintLogger");
 exports.PrintLogger = PrintLogger_1.PrintLogger;
+var LogLevel_1 = require("./src/LogLevel");
+exports.LogLevel = LogLevel_1.LogLevel;
 //# sourceMappingURL=index.js.map
